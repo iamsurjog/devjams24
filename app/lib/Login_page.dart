4726b1202cea5c0991c7f639af8fa7ebe72e1616
ANANYASHAH29 <156574916+ANANYASHAH29@users.noreply.github.com>
2024-09-21 23:48:07 +0530

    Update Login_page.dart

--- app/lib/Login_page.dart
+++ app/lib/Login_page.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'Input_details.dart';
-import 'Sign_in.dart'; // Only keep necessary imports
-
+import 'ScriptGeneratorScreen.dart';
+import 'Sign_in.dart';
+import 'main.dart';
 class LoginPage extends StatefulWidget {
   final String registeredEmail;
   final String registeredPassword;
@@ -20,7 +21,6 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void dispose() {
-    // Dispose controllers to prevent memory leaks
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
@@ -29,164 +29,160 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( // Use SafeArea to avoid overlap with system UI
+      body: SafeArea(
         child: Container(
+          height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
+            color: Colors.black,
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [
-                Colors.black, // First shade
-                Color(0xFF303030), // Second shade
+                Colors.black,
+                Color(0xFF303030),
               ],
             ),
           ),
-          child: SingleChildScrollView( // Allow scrolling in case of overflow
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Header and title part
-                  SizedBox(height: 40),
-                  Text(
-                    'ImaGen',
-                    style: GoogleFonts.montez(
-                      textStyle: TextStyle(
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 0),
+                Text(
+                  'ImaGen',
+                  style: GoogleFonts.montez(
+                    textStyle: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                  SizedBox(height: 80), // Space before the main content
-
-                  RichText(
-                    textAlign: TextAlign.left,
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Ready to Continue?\n\n',
-                          style: TextStyle(
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.white,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Enter your Account',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.normal,
-                            fontStyle: FontStyle.italic,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.person, color: Colors.pinkAccent),
-                      labelText: 'Email',
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: Colors.white, width: 2.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: Colors.pinkAccent, width: 2.0),
-                      ),
-                      labelStyle: TextStyle(color: Colors.white),
-                    ),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 35),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock, color: Colors.pinkAccent),
-                      labelText: 'Password',
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: Colors.white, width: 2.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: Colors.pinkAccent, width: 2.0),
-                      ),
-                      labelStyle: TextStyle(color: Colors.white),
-                    ),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 50),
-                  Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Colors.pinkAccent, Colors.deepPurpleAccent],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                        ),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: TextButton(
-                        onPressed: () {
-                          String email = emailController.text;
-                          String password = passwordController.text;
-
-                          if (email == widget.registeredEmail &&
-                              password == widget.registeredPassword) {
-                            // Successful login, navigate to HomePage
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => PunchInPage()),
-                            );
-                          } else {
-                            // Show error message
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('Invalid email or password')),
-                            );
-                          }
-                        },
-                        child: Container(
-                          constraints: BoxConstraints(
-                            maxWidth: 290.0,
-                            minHeight: 40.0,
-                          ),
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white, fontSize: 18.0),
-                          ),
+                ),
+                SizedBox(height: 50),
+                RichText(
+                  textAlign: TextAlign.left,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Ready to Continue?\n\n',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
+                      TextSpan(
+                        text: 'Enter your Account',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.normal,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 20),
-                  Center(
+                ),
+                SizedBox(height: 30.0),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person, color: Colors.pinkAccent),
+                    labelText: 'Email',
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: Colors.pinkAccent, width: 2.0),
+                    ),
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 35),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock, color: Colors.pinkAccent),
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: Colors.pinkAccent, width: 2.0),
+                    ),
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 50),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.pinkAccent, Colors.deepPurpleAccent],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
                     child: TextButton(
                       onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => SignUpPage()),
-                        );
+                        String email = emailController.text;
+                        String password = passwordController.text;
+
+                        if (email == widget.registeredEmail &&
+                            password == widget.registeredPassword) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => PunchInPage()),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Invalid email or password')),
+                          );
+                        }
                       },
-                      child: Text(
-                        'Do not have an account? Sign up',
-                        style: TextStyle(color: Colors.white),
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: 290.0,
+                          minHeight: 40.0,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      'Do not have an account? Sign up',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
