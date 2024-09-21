955725aeb004f68e6901899c802629a641c0b643
ANANYASHAH29 <156574916+ANANYASHAH29@users.noreply.github.com>
2024-09-21 22:59:34 +0530

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:first_project/feedback.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'Information_page.dart';
+import 'Login_page.dart';
 import 'ScriptGeneratorScreen.dart';
+import 'Sign_in.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,308 +15,217 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Sign Up/Login',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Splash Screen',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(
-        registeredEmail: 'Email',
-        registeredPassword: 'Password',
-      ),
+      home: MainPage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  final String registeredEmail;
-  final String registeredPassword;
-
-  LoginPage({required this.registeredEmail, required this.registeredPassword});
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-        children: [
-          // RichText widget to show different font sizes for text
-          Padding(
-            padding:
-                const EdgeInsets.only(top: 60.0), // Adjust this value as needed
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'VerbaGen\n\n',
-                    style: TextStyle(
-                      fontSize: 26.0, // Smaller font size for "AI Powered"
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'Login to your\nAccount',
-                    style: TextStyle(
-                      fontSize: 44.0, // Larger font size for "ScriptGen"
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black, // Darker shade at the top
+              Color(0xFF303030), // Gradient transition shade
+            ],
+          ),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Text (
+                'ImaGen',
+                style: GoogleFonts.montez(
+                  textStyle:
+                TextStyle(
+                  fontSize: 90.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
               ),
             ),
-          ),
-          SizedBox(height: 70.0),
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person), // Lock icon for password
-
-                labelText: 'Email'),
-          ),
-          TextField(
-            controller: passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock), // Lock icon for password
-
-                labelText: 'Password'),
-          ),
-          SizedBox(height: 70),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.purple,
-              padding: EdgeInsets.symmetric(horizontal: 120.0, vertical: 15.0),
-              textStyle: TextStyle(fontSize: 18.0),
+            SizedBox(height: 10),
+            Text(
+              'Scripts Simplified!',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
             ),
-            onPressed: () {
-              String email = emailController.text;
-              String password = passwordController.text;
 
-              if (email == registeredEmail && password == registeredPassword) {
-                // Successful login, navigate to HomePage
+            SizedBox(height: 75),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.zero,
+                textStyle: TextStyle(fontSize: 18.0),
+
+              ),
+
+              onPressed: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              } else {
-                // Show error message and option to go back to Sign Up Page
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Invalid email or password')),
-                );
-              }
-            },
-            child: Text('Login'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpPage()),
-              );
-            },
-            child: Text('Do not have an account? Sign up'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SignUpPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  bool _validateEmail(String email) {
-    final emailPattern = RegExp(r'^[a-zA-Z0-9._%+-]+@scriptgen\.com$');
-    return emailPattern.hasMatch(email);
-  }
-
-  bool _validatePassword(String password) {
-    final passwordPattern = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$');
-    return passwordPattern.hasMatch(password);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-          children: [
-            // RichText widget to show different font sizes for text
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 60.0), // Adjust this value as needed
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'VerbaGen\n\n',
-                      style: TextStyle(
-                        fontSize: 26.0, // Smaller font size for "AI Powered"
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Sign in to your\nAccount',
-                      style: TextStyle(
-                        fontSize: 44.0, // Larger font size for "ScriptGen"
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );// Add your functionality for Login
+              },
+              child: Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.pinkAccent, Colors.deepPurpleAccent], // Define your gradient colors here
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0), // Optional: Rounded corners
+                ),
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: 290.0, // Adjust the width of the button
+                    minHeight: 50.0, // Adjust the height of the button
+                  ),
+                  alignment: Alignment.center,
+                  child: Text('Get Started'),
                 ),
               ),
             ),
 
-            SizedBox(height: 70.0),
-
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person), // Icon for username
-
-                  labelText: 'Email'),
+            SizedBox(height: 30),
+            Text(
+              'or',
+              style: TextStyle(
+                  color: Colors.white
+              ),
             ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock), // Lock icon for password
-
-                  labelText: 'Password'),
-            ),
-            SizedBox(height: 70),
+            SizedBox(height: 30),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
+                elevation: 0,
                 foregroundColor: Colors.white,
-                backgroundColor: Colors.purple,
-                padding:
-                    EdgeInsets.symmetric(horizontal: 120.0, vertical: 15.0),
+                padding: EdgeInsets.zero,
                 textStyle: TextStyle(fontSize: 18.0),
+
               ),
+
               onPressed: () {
-                String email = emailController.text;
-                String password = passwordController.text;
-
-                if (!_validateEmail(email)) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text(
-                            'Email must be in the format name@scriptgen.com')),
-                  );
-                } else if (!_validatePassword(password)) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text(
-                            'Password must contain at least one uppercase letter, one lowercase letter, and one special character')),
-                  );
-                } else {
-                  // Navigate to Login Page after registration
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginPage(
-                        registeredEmail: email,
-                        registeredPassword: password,
-                      ),
-                    ),
-                  );
-                }
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage(registeredEmail: 'Email', registeredPassword: 'Password',)),
+                );// Add your functionality for Login
               },
-              child: Text('Register'),
-            ),
-          ]),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-        children: [
-          // RichText widget to show different font sizes for text
-          Padding(
-            padding:
-                const EdgeInsets.only(top: 60.0), // Adjust this value as needed
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'AI Powered\n',
-                    style: TextStyle(
-                      fontSize: 26.0, // Smaller font size for "AI Powered"
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                    ),
+              child: Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.pinkAccent, Colors.deepPurpleAccent], // Define your gradient colors here
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
                   ),
-                  TextSpan(
-                    text: 'VerbaGen',
-                    style: TextStyle(
-                      fontSize: 54.0, // Larger font size for "ScriptGen"
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+                  borderRadius: BorderRadius.circular(30.0), // Optional: Rounded corners
+                ),
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: 290.0, // Adjust the width of the button
+                    minHeight: 50.0, // Adjust the height of the button
                   ),
-                ],
-              ),
+                  alignment: Alignment.center,
+              child: Text('Login'),
             ),
-          ),
-          SizedBox(height: 20.0), // Space between text and image
-
-          // Image below the text
-          Image.asset(
-            'assets/Screenshot 2024-07-31 200751.png',
-            width: 520.0, // Adjust the size as needed
-            height: 520.0,
-          ),
-
-          SizedBox(height: 15.0), // Space between image and button
-
-          // Generate button below the image
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Generator()),
-              );
-              // Add your onPressed functionality here
-            },
-            child: Text('Get Started'),
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.purple,
-              padding: EdgeInsets.symmetric(horizontal: 120.0, vertical: 15.0),
-              textStyle: TextStyle(fontSize: 18.0),
             ),
-          ),
-        ],
-      ),
-    );
+            ),
+
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => AboutUs()),// Navigate to About Us
+                    );},
+                  child: Text(
+                    'About Us',
+                    style: TextStyle(
+                        decoration: TextDecoration.combine([
+                          TextDecoration.underline,
+                        ]),
+                        decorationColor: Colors.white,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => thoughts()),// Navigate to About Us
+                    );// Navigate to Share your thoughts
+                  },
+                  child: Text(
+                    'Share your thoughts',
+                    style: TextStyle(
+                        decoration: TextDecoration.combine([
+                          TextDecoration.underline,
+
+                        ]),
+                        decorationColor: Colors.white,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            ],
+        )
+        ),
+      );
+
   }
 }
 
-class Generator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'YouTube Script Generator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+Widget _buildGradientButton(BuildContext context, String text, VoidCallback onPressed) {
+  return Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Colors.pink, Colors.purple], // Gradient colors
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
       ),
-      home: ScriptGeneratorScreen(),
-    );
-  }
+      borderRadius: BorderRadius.circular(30), // Rounded corners
+    ),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 100.0, vertical: 15.0),
+        backgroundColor: Colors.transparent, // Transparent background
+        shadowColor: Colors.transparent, // Remove shadow for better gradient effect
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    ),
+  );
 }
+
+
+
+
